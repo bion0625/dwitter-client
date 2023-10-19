5560a34be05b08f206d4f89248cd723a2902c58a
bion0625 <stxtory@gmail.com>
2023-10-19 16:16:44 +0900

    init

---
+++ src/service/tweet.js
@@ -0,0 +1,49 @@
+export default class TweetService {
+
+  constructor(http, tokenStorage, socket){
+    this.http = http;
+    this.tokenStorage = tokenStorage;
+    this.socket = socket;
+  }
+  async getTweets(username) {
+    const query = username ? `?username=${username}` : '';
+    return this.http.fetch(`/tweets${query}`,{
+      method: 'GET',
+      headers: this.getHeader(),
+    });
+  }
+
+  async postTweet(text) {
+    return this.http.fetch(`/tweets`,{
+      method: 'POST',
+      headers: this.getHeader(),
+      body: JSON.stringify({text, username: 'ellie', name: 'Ellie'})
+    });
+  }
+
+  async deleteTweet(tweetId) {
+    return this.http.fetch(`/tweets/${tweetId}`,{
+      headers: this.getHeader(),
+      method: 'DELETE',
+    });
+  }
+
+  async updateTweet(tweetId, text) {
+    return this.http.fetch(`/tweets/${tweetId}`,{
+      method: 'PUT',
+      headers: this.getHeader(),
+      body: JSON.stringify({text})
+    });
+  }
+
+  getHeader(){
+    const token = this.tokenStorage.getToken();
+    return {
+      'Authorization': `Bearer ${token}`,
+    };
+  }
+
+  onSync(callback){
+    return this.socket.onSync('tweets', callback);
+  }
+}
